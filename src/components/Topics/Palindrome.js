c753ed5d618ce8a110c748af7be69f33ee64d662
Ashley Vick <ashleyvick@github.com>
2021-03-05 22:45:23 -0700

    Do not understand the palindrome section one bit. Just need to complete Sum.

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -4,13 +4,39 @@ class Palindrome extends React.Component {
     constructor(){
         super();
         this.state = {
-
+            userInput: '',
+            palindrome: '',
         };
     }
 
+    handleChange(value) {
+        this.setState({userInput: value});
+    }
+
+    isPalindrome (userInput){
+        const forwards = userInput;
+        const backwards = userInput;
+        backwards = backwards.split(''); // used to split a string into an array of substrings, and returns the new array
+        backwards = backwards.reverse(); //reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
+        backwards = backwards.join(''); // returns the array as a string
+    
+        if ( forwards === backwards ) {
+        this.setState({ palindrome: 'true' });
+        } else {
+        this.setState({ palindrome: 'false' });
+        }
+    }
+
+
     render (){
         return(
-            <p>Palindrome</p>
+            <div className="puzzleBox filterStringPB" >
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClick={ () => this.isPalindrome(this.state.userInput)}>Check</button>
+                <span className="resultsBox">Palindrome: { this.state.palindrome }</span>
+
+            </div>
         )
     }
 }
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -4,12 +4,34 @@ class Sum extends React.Component{
     constructor(){
         super ();
             this.state = {
+                number1: 0,
+                number2: 0,
+                sum: null
         };
+    }
+
+    updateNumber1() {
+        
+    }
+
+    updateNumber2 (){
+
+    }
+
+    add(num1, num2){
+        
     }
 
     render (){
         return(
-            <p>Sum</p>
+            <div className="puzzleBox sumPB" >
+                <h4>Sum</h4>
+                <input className="inputLine"  onChange={(e) => this.updateNumber1(e.target.value)}/>
+                <input className="inputLine"  onChange={(e) => this.updateNumber2(e.target.value)}/>
+                <button className="confirmationButton">Add</button>
+                <span className="resultsBox">Sum: {this.state.sum} </span>
+
+            </div>
         )
     }
 }
